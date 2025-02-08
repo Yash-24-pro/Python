458a602d12a7448457385b9b1e6d5383da18b28c
Yash Tripathi <yashtripathi2400@gmail.com>
2025-02-08 21:51:34 +0530

    Add files via upload

---
+++ Garde calculator.py
@@ -0,0 +1,12 @@
+grade = input("Enter characters O/A/B/C/F: ")
+if grade == "O":
+   print("Outstanding")
+elif grade == "A":
+   print("Very Good")
+elif grade == "B":
+   print("Good")
+elif grade == "C":
+   print("Average")
+else:
+   print("Fail")
+   
