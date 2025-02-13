86174293c7381f38fd6d250856afb8d528ed6db9
Yash Tripathi <yashtripathi2400@gmail.com>
2025-02-13 21:59:39 +0530

    Update Addition using function.py

--- Addition using function.py
+++ Addition using function.py
@@ -4,7 +4,7 @@ def add_binary(bin1, bin2):
     num2 = int(bin2, 2)
     sum_num = num1 + num2
     
-    # Convert the sum back to binary and return (remove '0b' prefix)
+ # Convert the sum back to binary and return (remove '0b' prefix)
     return bin(sum_num)[2:]
 
 # Input two binary numbers
